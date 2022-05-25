67d8dd9d639ec938a4974dc4d5a91edf5f5e06e8
Enmanuel <enmanueljosue2612@hotmail.com>
2022-05-25 18:21:37 -0400

    changes with use factory, http module and http service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,44 @@
 import { Module } from '@nestjs/common';
+import { HttpService, HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
+import { firstValueFrom } from 'rxjs';
+
+const API_KEY = '123456';
+const API_KEY_PROD = '654321';
 
 @Module({
-  imports: [ProductsModule, UsersModule],
+  imports: [
+    ProductsModule,
+    UsersModule,
+    HttpModule.registerAsync({
+      useFactory: () => ({
+        timeout: 1000,
+        maxRedirects: 5,
+      }),
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+    {
+      provide: 'TASKS',
+      inject: [HttpService],
+      useFactory: async (http: HttpService) => {
+        const tasks = await http.get(
+          'https://jsonplaceholder.typicode.com/todos',
+        );
+        const value = Promise.resolve(firstValueFrom(tasks));
+        return value;
+      },
+    },
+  ],
 })
 export class AppModule {}
