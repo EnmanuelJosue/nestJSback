303f4f92453dc16da6e3f1838bbbf5255c0161aa
Enmanuel <enmanueljosue2612@hotmail.com>
2022-05-28 13:56:02 -0400

    config migrations for database, to create, run, show and delete

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -21,7 +21,7 @@ const API_KEY_PROD = '654321';
           username: user,
           password,
           database: dbName,
-          synchronize: true,
+          synchronize: false,
           autoLoadEntities: true,
         };
       },
