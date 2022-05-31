a2673b1d4ef8e0d5998c4b21b7b2b20487d96b7a
Enmanuel <enmanueljosue2612@hotmail.com>
2022-05-31 17:33:09 -0400

    add decorator for public endpoints in app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,20 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './auth/decorators/public.decorator';
 import { ApiKeyGuard } from './auth/guards/api-key.guard';
+@UseGuards(ApiKeyGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Public()
   getHello(): string {
     return this.appService.getHello();
   }
 
   @Get('nuevo')
-  @UseGuards(ApiKeyGuard)
+  @Public()
   newEndpoint() {
     return 'Yo soy nuevo';
   }
