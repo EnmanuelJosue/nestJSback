aeaaa71462aa17f7dba462b3ccf49cddcd22b785
Enmanuel <enmanueljosue2612@hotmail.com>
2022-06-02 18:56:12 -0400

    changes package json for run migrations in prod

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -14,6 +14,7 @@ const API_KEY_PROD = '654321';
       inject: [config.KEY],
       useFactory: (configService: ConfigType<typeof config>) => {
         return {
+          entities: ['dist/**/*.entity{.ts,.js}'],
           type: 'postgres',
           url: configService.postgresUrl,
           synchronize: false,
